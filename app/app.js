b795fefbbbcfaf7aeab5f5f3b4595b3a17ea34f4
Thomics <contot@gmail.com>
2016-06-30 18:36:21 -0700

    working with routes

--- app/app.js
+++ app/app.js
@@ -8,8 +8,13 @@ app.config(["$routeProvider", function( $routeProvider ) {
 
   $routeProvider
       .when('/', {
-        templateUrl: '../public/templates/sign-on.html'
+        templateUrl: 'public/templates/sign-on.html'
+      })
+      .when('/select-screen', {
+        templateUrl: 'public/templates/select-screen.html'
+      })
+      .when('/display-board', {
+        templateUrl: 'public/templates/display-board.html'
       });
 
 }]);
-
