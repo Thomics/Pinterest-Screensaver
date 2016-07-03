92ecbcbfc280cab60509289750d06888d6c028bd
Thomics <contot@gmail.com>
2016-07-02 20:52:37 -0700

    trying some data.access_token

--- app/scripts/services/sign-on.js
+++ app/scripts/services/sign-on.js
@@ -17,13 +17,14 @@ angular.module('Pinterest')
       $http.post('https://api.pinterest.com/v1/oauth/token?grant_type=authorization_code&client_id=4811218831118712860&client_secret=151c65575b58e9783627c80743eee6dba0bab7fd967ab4533c2cc3d87b2d5b49&code=' + token)
         .then(function(data) {
           console.log(data);
+          console.log(data.access_token);
           getBoard(data);
         });
 
     };
 
 
-    this.getBoard = function( code ) {
+    this.getBoard = function( token ) {
 
       $http.get('https://api.pinterest.com/v1/me/boards/?access_token=' + token + '&fields=id%2Curl%2Cname')
         .then(function( data ) {
