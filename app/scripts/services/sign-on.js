5275c533879587ac318499ae70628676d709a885
Thomics <contot@gmail.com>
2016-07-02 21:52:40 -0700

    working post, now lets have a working get

--- app/scripts/services/sign-on.js
+++ app/scripts/services/sign-on.js
@@ -17,8 +17,9 @@ angular.module('Pinterest')
       $http.post('https://api.pinterest.com/v1/oauth/token?grant_type=authorization_code&client_id=4843083155244066954&client_secret=c3070903c5fbe1daa699846708c06d0f4f5713351b97d2a5f357b9bc3e541a16&code=' + token)
         .then(function(data) {
           console.log(data);
+          console.log(data.data.access_token);
           console.log(data.access_token);
-          getBoard(data);
+          getBoard(data.access_token);
         });
 
     };
