c2dcf2a53773ef6ca7c3a1b5c0524302c65d9f49
Thomics <contot@gmail.com>
2016-07-10 03:02:37 -0700

    working scrolling

--- app/scripts/controllers/display-boards.js
+++ app/scripts/controllers/display-boards.js
@@ -4,41 +4,61 @@ angular.module('pinterest')
   .controller('displayBoardCtrl', [ 'signOnService', "$scope", '$location', '$interval',
     function( signOnService, $scope, $location, $interval){
 
-      //$scope.high = '10000px';
-
       $scope.pins = signOnService.pins;
 
-      console.log(signOnService.pins);
+      var timer;
+
+      $scope.paused = false;
+
       $scope.counter = 0;
+
 
 
       $scope.incrementCounter = function( ) {
         $scope.counter += 1;
       };
 
-      console.log(signOnService.seconds);
-      console.log($scope.seconds);
 
-      $interval($scope.incrementCounter, (signOnService.seconds * 1000), $scope.pins.length);
+      $scope.start = function() {
+        $scope.paused = false;
+        timer = $interval($scope.incrementCounter, (signOnService.seconds * 1000), $scope.pins.length);
+      };
 
+
+      $scope.stop = function() {
+        $scope.paused = true;
+
+        $interval.cancel(timer);
+
+        timer = undefined;
+      };
 
 
       $scope.windowScroll = function() {
 
-        $('html, body').animate({
-          scrollTop: ($scope.pins[counter].image.original.height) + $(window).height()
-        }, ((signOnService.seconds * 1000)/2));
+        if( !$scope.paused ) {
 
-        $('html, body').animate({
-          scrollTop: 0
-        }, ((signOnService.seconds * 1000)/2));
+          $('html, body').animate({
+            scrollTop: ($scope.pins[$scope.counter].image.original.height) + $(window).height()
+          }, ((signOnService.seconds * 1000) / 2));
 
-        //Code from stackoverflow
-        $('html, body').bind("scroll mousedown DOMMouseScroll mousewheel keyup", function(event){
-          if ( event.which > 0 || event.type === "mousedown" || event.type === "mousewheel"){
-            $('html, body').stop().unbind('scroll mousedown DOMMouseScroll mousewheel keyup');
-          }
-        });
+          $('html, body').bind("scroll mousedown DOMMouseScroll mousewheel keyup", function (event) {
+            if (event.which > 0 || event.type === "mousedown" || event.type === "mousewheel") {
+              $('html, body').stop().unbind('scroll mousedown DOMMouseScroll mousewheel keyup');
+            }
+          });
+
+          $('html, body').animate({
+            scrollTop: 0
+          }, ((signOnService.seconds * 1000) / 2));
+
+          //Code from stackoverflow
+          $('html, body').bind("scroll mousedown DOMMouseScroll mousewheel keyup", function (event) {
+            if (event.which > 0 || event.type === "mousedown" || event.type === "mousewheel") {
+              $('html, body').stop().unbind('scroll mousedown DOMMouseScroll mousewheel keyup');
+            }
+          });
+        }
       };
 
 
---
+++ app/scripts/directives/play.js
@@ -0,0 +1,15 @@
+//'use strict';
+//
+//angular.module('pinterest')
+//  .directive('play', function(){
+//    return {
+//      templateUrl: 'public/templates/play.html',
+//      restrict: 'E',
+//      replace: true
+//      //link: function() {
+//      //  $('.play').on('click', function(e) {
+//      //    e.preventDefault();
+//      //  });
+//      //}
+//    }
+//  });
